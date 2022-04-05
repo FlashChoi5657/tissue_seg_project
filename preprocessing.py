b6fd7068f6d57c8596c4ce19e7f359cad6d6df4c
FlashChoi5657 <minhyeok@image.cau.ac.kr>
2022-04-05 20:55:06 +0900

    model test

--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,17 @@ from network.resunet_3d import *
 import segmentation_models_pytorch as smp
 
 import pytorch3dunet.unet3d.model as p
+import torch
+import pytorch_model_summary as su
 
-a=p.ResidualUNet3D()
-b=smp.UnetPlusPlus()
+model=p.ResidualUNet3D(in_channels=2, out_channels=2)
+print(model)
+
+print(su.summary(model,torch.zeros((16,2,16,128,128)), show_parent_layers=True))
+
+
+model2d=smp.UnetPlusPlus(in_channels=2)
+print(model2d)
+
+
 
