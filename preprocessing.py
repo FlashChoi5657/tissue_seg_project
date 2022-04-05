16fc02173ecb2f406b3e5b450ace993706bf886e
FlashChoi5657 <minhyeok@image.cau.ac.kr>
2022-04-05 19:45:50 +0900

    2d pytorch model

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,13 @@
 import os, sys
-sys.path.append('/home/check5657/Workspace/pycharm_pytorch')
-
+# sys.path.append('/home/check5657/Workspace/pycharm_pytorch')
 import mh_overlay, mh_loader
 from network.resunet_3d import *
+import segmentation_models_pytorch as smp
 
+import pytorch3dunet.unet3d.model
+
+
+
+
+a=smp.UnetPlusPlus()
 
